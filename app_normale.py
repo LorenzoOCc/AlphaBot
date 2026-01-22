fd277bc04624d48f34bb596f1994e1097164a5fe
RTX-pi3tr0-07 <pietro.viale2@itiscuneo.eu>
2026-01-22 10:28:08 +0100

    Add files via upload

---
+++ app_normale.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+from flask_login import (
+    LoginManager ,UserMixin,
+    login_user, login_required,
+)
+import AlphaBot
+
+app = Flask(__name__)
+robot = AlphaBot.AlphaBot()
+robot.stop()
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        if "Avanti" in request.form:
+            robot.forward()
+        elif "Indietro" in request.form:
+            robot.backward()
+        elif "Destra" in request.form:
+            robot.right()
+        elif "Sinistra" in request.form:
+            robot.left()
+        elif "Stop" in request.form:
+            robot.stop()
+        
+        # IMPORTANTE: devi sempre restituire una risposta
+        return render_template("index.html")
+    
+    # GET request
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=False, host="0.0.0.0")
